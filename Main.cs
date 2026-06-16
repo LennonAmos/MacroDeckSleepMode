64fbcee9fe7e809d5a64f1dc7663734a53964975
LennonAmos <LennonAmos@users.noreply.github.com>
2026-06-16 23:14:23 +0100

    Remove manual plugin update registration

--- Main.cs
+++ Main.cs
@@ -1,22 +1,9 @@
 using SuchByte.MacroDeck.Plugins;
-using System.Reflection;
 
 namespace MacroDeckSleepMode;
 
 public sealed class Main : MacroDeckPlugin
 {
-    public Main()
-    {
-        SetPluginMetadata("Name", "MacroDeck Sleep Mode");
-        SetPluginMetadata("Version", "1.0.3");
-        SetPluginMetadata("Author", "lenno");
-
-        if (!PluginManager.UpdatedPlugins.Contains(this))
-        {
-            PluginManager.UpdatedPlugins.Add(this);
-        }
-    }
-
     public override bool CanConfigure => true;
 
     public override void OpenConfigurator()
@@ -33,12 +20,5 @@ public sealed class Main : MacroDeckPlugin
         {
             new GoToSleepProfileAction()
         };
-    }
-
-    private void SetPluginMetadata(string propertyName, string value)
-    {
-        typeof(MacroDeckPlugin)
-            .GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-            ?.SetValue(this, value);
     }
 }
